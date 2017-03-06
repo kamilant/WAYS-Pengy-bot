fd8131b9d25700199e488390a6c9583cee2757bc
Kamil Antropik <kamil.antropik@eyedea.io>
2017-03-06 02:27:35 +0100

    chore(env) add env variables

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('working');
+});
+server.listen(3000);
+
 if (!process.env.SLACKBOT_TOKEN) {
     console.log('Error: Specify token in environment');
     process.exit(1);
