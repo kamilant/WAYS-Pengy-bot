2393d5a4d64c4b0d164297d7e530dc62b5a4d8f4
Kamil Antropik <kamil.antropik@eyedea.io>
2017-03-06 02:46:27 +0100

    fix(yaml)

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('working');
 });
-server.listen(3000);
-
-if (!process.env.SLACKBOT_TOKEN) {
+server.listen(3000, () => {
+	if (!process.env.SLACKBOT_TOKEN) {
     console.log('Error: Specify token in environment');
     process.exit(1);
 }
@@ -219,3 +218,4 @@ function formatUptime(uptime) {
     uptime = uptime + ' ' + unit;
     return uptime;
 }
+});
